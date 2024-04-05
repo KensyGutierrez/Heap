f1ec5a671f095cfe14d492bd8e6e85c495b7a8d4
KensyGutierrez <160553926+KensyGutierrez@users.noreply.github.com>
2024-04-05 12:49:19 -0600

    main.cpp
    modificación de main

--- main.cpp
+++ main.cpp
@@ -1,15 +1,27 @@
+#include "TreeHeap.hpp"
 #include <iostream>
-#include "Nodo.hpp"
-#include<vector>
 
 using namespace std;
-
-int main()
-{
-  vector<Nodo> arbol;
-  vector<Nodo>newTree()=arbol;
-  monstrarArbol(arbol);
-  cout<<" La altura del arbol  es :  "<< Height(arbol)<<endl;
-  
-  return 0;
+int main(){
+	int n=6;
+	int l=4;
+	int A[n]={10,80,100,10,5,25};
+	
+	
+	TreeHeap H(n);
+	for(int i=0;i<n;i++){
+		H.insertar(A[i]);
+	}
+    H.imprimir();
+    
+    cout<<"__________________________________________________________________"<<endl;
+    cout<<"Obtendremos el maximo de nuestro Heap"<<endl;
+    H.obtener_Max();
+    H.imprimir(); //imprimimos nuestro Heap
+    cout<<"__________________________________________________________________"<<endl;
+    H.insertar(92); 
+    H.imprimir();
+    H.insertar(34);
+   
+ return 0;
 }
